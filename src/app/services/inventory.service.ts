84b56d6c5ed6800ab4e797380dd239f53f5736b1
kano521520 <2258939528@qq.com>
2026-04-23 17:03:21 +0800

    Xuanhao: Fixed HttpClient provider and enabled Ionic standalone services
    Configured provideHttpClient in main.ts/app.config.ts to resolve NG0201 error.
    Implemented core RESTful API methods in InventoryService.

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { InventoryItem } from '../models/inventory.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Inventory {
-  
+export class InventoryService {
+  private readonly API_URL = 'https://prog2005.it.scu.edu.au/ArtGalley';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<InventoryItem[]> {
+    return this.http.get<InventoryItem[]>(this.API_URL);
+  }
+
+  getByName(name: string): Observable<InventoryItem> {
+    return this.http.get<InventoryItem>(`${this.API_URL}/${name}`);
+  }
+
+  add(item: any): Observable<any> {
+    return this.http.post(this.API_URL, item);
+  }
+
+  update(name: string, item: any): Observable<any> {
+    return this.http.put(`${this.API_URL}/${name}`, item);
+  }
+
+  delete(name: string): Observable<string> {
+    return this.http.delete(`${this.API_URL}/${name}`, { responseType: 'text' });
+  }
 }
