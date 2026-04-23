08aa6b16b75e4c25a40102d08f75b9ea638864fe
kano521520 <2258939528@qq.com>
2026-04-23 17:04:14 +0800

    Xuanhao: Corrected Tab routing paths and standalone component imports
    Fixed routing errors by ensuring correct file extensions (.component vs .page).
    Linked the 4 main functional components to the ion-tabs bar.

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,30 +7,31 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+        path: 'inventory-list',
+        loadComponent: () => import('../inventory-list/inventory-list.component').then((m) => m.InventoryListComponent),
       },
       {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+        path: 'add-featured',
+        loadComponent: () => import('../add-featured/add-featured.component').then((m) => m.AddFeaturedComponent),
       },
       {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+        path: 'edit-delete',
+        loadComponent: () => import('../edit-delete/edit-delete.component').then((m) => m.EditDeleteComponent),
+      },
+      {
+        path: 'privacy-security',
+        loadComponent: () => import('../privacy-security/privacy-security.component').then((m) => m.PrivacySecurityComponent),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/inventory-list',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/inventory-list',
     pathMatch: 'full',
   },
 ];
